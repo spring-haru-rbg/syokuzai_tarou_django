ba9a6562118c9cbf05fec014efc0ff31931d6d5d
spring-haru-rbg <18fi080@ms.dendai.ac.jp>
2020-12-04 09:54:51 +0900

    レシピ_削除ページ遷移_1204

--- syokuzai_tarou/refrigerator/urls.py
+++ syokuzai_tarou/refrigerator/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('food_register',views.food_register , name = 'food_register'),
     path('food_change_select',views.food_change_select , name = 'food_change_select'),
     path('food_search',views.food_search , name = 'food_search'),
+    path('recipe_select',views.recipe_select , name = 'recipe_select'),
+    path('food_delete',views.food_delete , name = 'food_delete'),
+
 ]
