51e6e0c02f000153c268fad9637bec53a1d6546b
spring-haru-rbg <18fi080@ms.dendai.ac.jp>
2020-12-04 22:36:18 +0900

    画面遷移追加_1204

--- syokuzai_tarou/refrigerator/views.py
+++ syokuzai_tarou/refrigerator/views.py
@@ -59,9 +59,33 @@ def food_change_select(request):
         'goto_recipe_select_text' : 'レシピ表示',
         'goto_delete' : 'food_delete',
         'goto_delete_text' : '削除',
+        'goto_change' : 'food_change',
+        'goto_change_text' : '食材変更',
 
     }
     return render(request, 'refrigerator/food_change_select.html',params)
+
+def food_change(request):
+    params = {
+        'title' : '数量変更',
+        'text' : '食材数量変更ページ',
+        'goto_refrigerator' : 'refrigerator',
+        'goto_refrigerator_text' : '食材一覧',
+        'goto_register' : 'food_register',
+        'goto_register_text' : '登録',
+        'goto_change_select' : 'food_change_select',
+        'goto_change_select_text' : '変更',
+        'goto_search' : 'food_search',
+        'goto_search_text' : '検索',
+        'goto_recipe_select' : 'recipe_select',
+        'goto_recipe_select_text' : 'レシピ表示',
+        'goto_delete' : 'food_delete',
+        'goto_delete_text' : '削除',
+        'goto_change_refrigerator' : 'refrigerator',
+        'goto_change_refrigerator_text' : '数量変更',
+
+    }
+    return render(request, 'refrigerator/food_change.html',params)
 
 def food_search(request):
     params = {
@@ -99,8 +123,33 @@ def recipe_select(request):
         'goto_recipe_select_text' : 'レシピ表示',
         'goto_delete' : 'food_delete',
         'goto_delete_text' : '削除',
+        'goto_recipe' : 'recipe',
+        'goto_recipe_text' : 'レシピ検索',
     }
     return render(request, 'refrigerator/recipe_select.html',params)
+
+def recipe(request):
+    params = {
+        'title' : 'レシピ検索結果表示',
+        'text' : 'レシピ検索結果表示ページ',
+        'goto_refrigerator' : 'refrigerator',
+        'goto_refrigerator_text' : '食材一覧',
+        'goto_register' : 'food_register',
+        'goto_register_text' : '登録',
+        'goto_change_select' : 'food_change_select',
+        'goto_change_select_text' : '変更',
+        'goto_search' : 'food_search',
+        'goto_search_text' : '検索',
+        'goto_recipe_select' : 'recipe_select',
+        'goto_recipe_select_text' : 'レシピ表示',
+        'goto_delete' : 'food_delete',
+        'goto_delete_text' : '削除',
+        'goto_recipe' : 'recipe',
+        'goto_recipe_text' : 'レシピ検索',
+        'goto_recipe_reselect' : 'recipe_select',
+        'goto_recipe_reselect_text' : 'レシピ検索し直す',
+    }
+    return render(request, 'refrigerator/recipe.html',params)
 
 def food_delete(request):
     params = {
@@ -118,7 +167,10 @@ def food_delete(request):
         'goto_recipe_select_text' : 'レシピ表示',
         'goto_delete' : 'food_delete',
         'goto_delete_text' : '削除',
+        'goto_delete_refrigerator' : 'refrigerator',
+        'goto_delete_refrigerator_text' : '食材削除',
     }
     return render(request, 'refrigerator/food_delete.html',params)
+
 
 
