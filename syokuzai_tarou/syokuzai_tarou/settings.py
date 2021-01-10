699f1be0b8cea86cd02a151ae97d7857c9e82c4d
h-cha <cocoa0331oreo@gmail.com>
2021-01-11 02:10:42 +0900

    vps_設定_20210111

--- syokuzai_tarou/syokuzai_tarou/settings.py
+++ syokuzai_tarou/syokuzai_tarou/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!fqx3xoujq-8wc(34ph+y&&988!62rx_gm_xkt5^$$qnr$-lk6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","160.251.12.146"]
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/share/nginx/html/static'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
